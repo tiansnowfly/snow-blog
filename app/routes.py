86ef9bd284d89e34693060e5b1d243893419bdc9
tiansnowfly <1737145412@qq.com>
2020-09-14 21:44:38 +0800

    add index.html

--- app/routes.py
+++ app/routes.py
@@ -10,20 +10,21 @@ from app.models import Post,User
 # 当一个没有登录的用户访问被`@login_required`装饰器保护的视图函数时，装饰器将重定向到登录页面
 # @login_required
 def login():
+    return render_template('index.html')
     # current_user变量来自Flask_Login，在处理过程任何时候调用都能获取当前用户对象
-    if current_user.is_authenticated: # 用户已经登录定位到主页
-        return redirect(url_for('login'))
-    form=LoginForm()
-    if form.validate_on_submit():
-        user=User.query.filter_by(username=form.username.data).first()
-        # 用户不存在或者密码错误
-        if user is None or not user.check_password(form.password.data):
-            flash("Invalid username or password")
-            return redirect(url_for('login'))
-        # login_user()函数将用户状态转为登录
-        login_user(user,remember=form.remember_me.data)
-        return redirect(url_for('login'))
-    return render_template('login.html',form=form)
+    # if current_user.is_authenticated: # 用户已经登录定位到主页
+    #     return redirect(url_for('login'))
+    # form=LoginForm()
+    # if form.validate_on_submit():
+    #     user=User.query.filter_by(username=form.username.data).first()
+    #     # 用户不存在或者密码错误
+    #     if user is None or not user.check_password(form.password.data):
+    #         flash("Invalid username or password")
+    #         return redirect(url_for('login'))
+    #     # login_user()函数将用户状态转为登录
+    #     login_user(user,remember=form.remember_me.data)
+    #     return redirect(url_for('login'))
+    # return render_template('login.html',form=form)
 
 # 用户登出
 @app.route('/logout')
